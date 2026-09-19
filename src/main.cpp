f50efba8ed785a6b3daf71d85b416c98dee4d11d
Sherbiny <mustafa.elsherbiny.ext@siemens.com>
2026-09-19 17:26:32 +0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 #include <string>
+enum Commands {
+    ExitCommand=0
+};
+enum status {
+    Exit,
+    Erorr
+};
+class CommandHandler {
 
+public:
+    status Processing(std::string command) {
+
+    }
+};
 int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
+  CommandHandler _commandHandler;
     while (true) {
 
         std::cout << "$ ";
