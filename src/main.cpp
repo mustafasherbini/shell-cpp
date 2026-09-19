4991b62fc5b4cc14a4e2f4a9261f720a2209205c
Sherbiny <mustafa.elsherbiny.ext@siemens.com>
2026-09-19 15:43:25 +0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,12 @@
 #include <string>
 
 int main() {
-  // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
    std::cout << "$ ";
     std :: string command;
     getline(std :: cin, command);
-    std :: cout<<command +": command not found";
+    std :: cout<<command +": command not found"<<std::endl;
 
 }
