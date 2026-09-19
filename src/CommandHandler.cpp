a8217fc4cab7725c937e349501a21a5a5ea66496
Sherbiny <mustafa.elsherbiny.ext@siemens.com>
2026-09-19 18:05:26 +0300

    codecrafters submit [skip ci]

--- src/CommandHandler.cpp
+++ src/CommandHandler.cpp
@@ -22,7 +22,7 @@ public:
     }
 
     static status EchoProcessing(const std::string& command) {
-     std::cout << command.substr(6);
+     std::cout << command.substr(5);
         return Done;
     }
 
