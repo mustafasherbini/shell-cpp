98f0d0cab7b7b091f1d271b92b8a141f22f9e58a
Sherbiny <mustafa.elsherbiny.ext@siemens.com>
2026-09-19 15:37:05 +0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,10 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+  std::cout << "$ ";
 
-  // TODO: Uncomment the code below to pass the first stage
-   std::cout << "$ ";
-    std :: string command;
-    getline(std :: cin, command);
-    std :: cout<<command +": command not found";
+  std :: string command;
+  getline(std :: cin, command);
+  std :: cout<<command +": command not found";
 
 }
