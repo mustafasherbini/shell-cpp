300dcb4f5b81ce395591354f3f27b97338217d96
Sherbiny <mustafa.elsherbiny.ext@siemens.com>
2026-09-19 16:16:17 +0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,12 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-    std::cout << "$ ";
-    std :: string command;
-    getline(std :: cin, command);
-    std :: cout<<command <<": command not found\r\n";
+    while (true) {
+
+        std::cout << "$ ";
+        std :: string command;
+        getline(std :: cin, command);
+        std :: cout<<command <<": command not found\r\n";
+    }
 
 }
