f7981915f3355c51d889c12de6c5f0f7491cf62e
Sherbiny <mustafa.elsherbiny.ext@siemens.com>
2026-09-26 02:16:05 +0300

    codecrafters submit [skip ci]

--- src/Commands/CustomCommand.cpp
+++ src/Commands/CustomCommand.cpp
@@ -9,8 +9,8 @@ void CustomCommand::Processing(const std::vector<std::string> &args) {
 
      for (int i = 0; i < args.size(); ++i) {
           command+=args[i];
-          if (i+1!=args.size() and args[i+1].front()!='/')
-               command+='/';
+          if (i+1!=args.size() )
+               command+=' ';
      }
      if (CommandBase::IsAnExecutable(getenv("PATH"), command, fileDir)) {
      std::system(command.c_str());
